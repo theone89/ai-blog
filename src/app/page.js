39628c2c40713b88bca5d267f85a38b79e5237be
theone8907 <franklincampos8907@gmail.com>
2024-08-02 17:59:22 -0400

    fix image

--- src/app/page.js
+++ src/app/page.js
@@ -284,7 +284,7 @@ export default function Home() {
 
   return (
     <div>
-      <div className="container mx-auto ">
+      <div className="container mx-auto px-4 ">
 
         {!generation ?
           <InfoApp />
