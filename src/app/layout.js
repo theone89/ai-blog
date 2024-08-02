5da9cbb6a751f776101ab0dd1090fec1fa7b759a
theone8907 <franklincampos8907@gmail.com>
2024-08-02 18:28:53 -0400

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ai Blog",
+  title: "Blog AI",
   description: "App generadora de contenido para un blog",
 };
 
