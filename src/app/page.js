722b0fa6c70c9e0caca11e420a93a6ebff5ea163
theone8907 <franklincampos8907@gmail.com>
2024-08-02 11:09:09 -0400

    modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   src/app/actions.js 	modified:   src/app/globals.css 	modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,253 @@
+"use client";
+
+import { useRef, useState, Fragment } from "react";
+import { generate } from "./actions";
+import { readStreamableValue } from "ai/rsc";
+import { z } from "zod";
 import Image from "next/image";
+import { createApi } from "unsplash-js";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+const api = createApi({
+  // Don't forget to set your access token here!
+  // See https://unsplash.com/developers
+  accessKey: "je80tOS5RE_JEfcfLZ_DB5hkxXa3H9qb4w3Yv27VzWM",
+});
+
+const PhotoComp = ({ photo, size, style }) => {
+  const { user, urls } = photo;
+
+  return (
+    <Fragment>
+      <Image
+        height={size.height}
+        width={size.width}
+        className={style}
+        src={urls.regular}
+        alt="una imagen"
+      />
+      <a
+        className=""
+        target="_blank"
+        href={`https://unsplash.com/@${user.username}`}
+      >
+        {user.name}
+      </a>
+    </Fragment>
+  );
+};
 
 export default function Home() {
+  const [data, setPhotosResponse] = useState();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [input, setInput] = useState("");
+  const [check, setCheck] = useState(false);
+  const codeRef = useRef(null);
+  const [copied, setCopied] = useState(false);
+  const [generation, setGeneration] = useState();
+
+  function handleChange(e) {
+    const { checked } = e.target;
+    setCheck(checked);
+  }
+
+  const copyCode = () => {
+    const code = codeRef.current?.innerText;
+    if (code) {
+      navigator.clipboard
+        .writeText(code)
+        .then(() => { })
+        .catch((err) => {
+          console.error("Error al copiar el código: ", err);
+        });
+    }
+    setCopied(!copied);
+  };
+
+  function getImagesAPi() {
+    api.search
+      .getPhotos({
+        query: `${generation?.blogs[0].images}`,
+        orientation: "landscape",
+        perPage: 4,
+      })
+      .then((result) => {
+        setPhotosResponse(result);
+      })
+      .catch(() => {
+        console.log("something went wrong!");
+      });
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div>
+      <div className="container mx-auto ">
+        {generation?.blogs?.map((blog, index) => (
+          <div key={index} className="prose lg:prose-xl mx-auto">
+            <div className="">
+              <div className="text-start p-4 ">
+                <h1 className="text-4xl md:text-6xl  font-bold ">
+                  {blog.title}
+                </h1>
+                <p className="text-xl md:text-4xl font-bold ">
+                  {blog?.subtitle}
+                </p>
+              </div>
+              <div className="mt-4">
+                <h3 className="text-sm font-semibold text-slate-500">
+                  Publicado el {blog?.date}
+                </h3>
+                <h3 className="text-xl font-semibold mb-2">{blog?.author}</h3>
+              </div>
+              <div className="mb-4">
+                {!data ? (
+                  "IsLoading..."
+                ) : (
+                  <PhotoComp
+                    size={{
+                      width: 500,
+                      height: 500,
+                    }}
+                    style={"w-full object-cover h-96 shadow-lg"}
+                    key={data?.response?.results[0].id}
+                    photo={data?.response?.results[0]}
+                  />
+                )}
+
+                <div className=" mx-auto">
+                  <p className="">Tags:</p>
+                  {blog?.keywords?.map((keyword, index) => (
+                    <span
+                      key={index}
+                      className="inline-block bg-blue-200 text-blue-800 p-1 m-1 rounded"
+                    >
+                      <a href="#">{keyword}</a>
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <article className="text-justify prose prose-gray mx-auto max-w-3xl py-12 dark:prose-invert">
+                <h1 className="text-2xl font-bold">Introduccion:</h1>
+                <p className="mb-4">{blog?.text?.introduction}</p>
+                <div className=" text-justify md:grid  md:grid-cols-2 gap-1 mx-2">
+                  <div className=" mr-2">
+                    {!data ? (
+                      "IsLoading..."
+                    ) : (
+                      <PhotoComp
+                        size={{
+                          width: 500,
+                          height: 500,
+                        }}
+                        style={" object-cover"}
+                        key={data?.response?.results[1].id}
+                        photo={data?.response?.results[1]}
+                      />
+                    )}
+                    <h1 className="text-2xl font-bold"></h1>
+                    <h1 className="text-2xl font-bold">Importante</h1>
+                    <p className="mb-4">{blog?.text?.development}</p>
+                  </div>
+                  <div>
+                    <p className="mb-4">{blog?.text?.development2}</p>
+                    {!data ? (
+                      "IsLoading..."
+                    ) : (
+                      <PhotoComp
+                        size={{
+                          width: 500,
+                          height: 500,
+                        }}
+                        style={" object-cover"}
+                        key={data?.response?.results[2].id}
+                        photo={data?.response?.results[2]}
+                      />
+                    )}
+                  </div>
+                </div>
+                <h1 className="text-2xl font-bold">Conclusiones:</h1>
+                <p className="mb-4">{blog?.text?.conclusions}</p>
+              </article>
+              {blog.images ? getImagesAPi() : "no hay imagen"}
+            </div>
+          </div>
+        ))}
+      </div>
+      {check == true ? (
+        <div className="bg-slate-200 text-black container mx-auto overflow-y-auto ">
+          <div className="ml-2 mt-2">
+            <button
+              onClick={copyCode}
+              className=" items-center justify-center whitespace-nowrap focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none border border-input font-medium rounded-md text-xs  right-4 top-5 z-10 flex h-6 gap-1 bg-white px-1.5 text-gray-500 shadow-none transition-colors hover:bg-gray-100 hover:text-gray-700 disabled:opacity-50"
+            >
+              {copied == true ? "Copiado" : "Copiar"}
+              {copied == true ? "✅" : "📋"}
+            </button>
+          </div>
+
+          <pre ref={codeRef} className=" p-4" id="code">
+            {JSON.stringify(generation, null, 2)}
+          </pre>
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="flex flex-col w-full max-w-md b py-24 mx-auto stretch">
+        <div className="fixed bottom-0 w-full  backdrop-blur-sm max-w-md  p-2 mb-8 border  border-gray-300 rounded shadow-xl">
+          <div className="flex justify-between ">
+            <input
+              type="text"
+              className="bg-transparent w-2/3 mr-1 rounded-e-md pl-1  border-orange-200 border-b-4 focus:outline-none hover:border-orange-300"
+              placeholder="¿De que trata el articulo?..."
+              value={input}
+              onChange={(event) => {
+                setInput(event.target.value);
+              }}
             />
-          </a>
+
+            <button
+              className={`bg-blue-700  p-1 rounded-md w-1/3 text-white disabled:bg-slate-500/20 `}
+              onClick={async () => {
+                const { object } = await generate(
+                  "Crea un blog sobre:" + input
+                );
+
+                for await (const partialObject of readStreamableValue(object)) {
+                  if (partialObject) {
+                    setGeneration(partialObject);
+                  }
+                }
+              }}
+              disabled={input == "" || isLoading ? true : false}
+            >
+              Enviar
+            </button>
+          </div>
+          <div>
+            {isLoading && (
+              <div>
+                <div>Loading...</div>
+                <button type="button" onClick={() => stop()}>
+                  Stop
+                </button>
+              </div>
+            )}
+            <div className="text-end ">
+              <label htmlFor="json"> JSON </label>
+              <input
+                type="checkbox"
+                name="json"
+                id="json"
+                checked={check}
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
